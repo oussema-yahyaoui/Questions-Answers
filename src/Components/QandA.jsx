25d25c9c45cc115b59647e60fc9bc85438dc60ac
oussema-yahyaoui <ousema.benabdallah.yahyaoui@gmail.com>
2021-10-17 18:28:37 +0100

    vue full done

--- src/Components/QandA.jsx
+++ src/Components/QandA.jsx
@@ -37,17 +37,17 @@ class QandA extends Component {
 
   render() {
     return (
-      <div className="min-h-screen   grid grid-cols-7  justify-center  ">
+      <div className=" min-h-screen   grid grid-cols-7  justify-center  ">
         <div className="col-span-1 "></div>
-        <div className="col-span-5  grid grid-rows-6">
-          <div className="row-span-1  ">
+        <div className=" col-span-5  grid grid-rows-6">
+          <div className=" row-span-1 px-80 ">
             QUESTIONS & ANSWERS
             <div className="flex items-center justify-center">
               <div className="flex border-2 ">
                 <input
                   className="text text-bold"
                   type="text"
-                  className="px-4 py-2 w-80"
+                  className="px-5 py-2 w-80"
                   placeholder="HAVE QUESTION?SERACH FOR ANSWER..."
                 />
                 <button className="flex items-center justify-center px-2 border-l">
@@ -65,59 +65,79 @@ class QandA extends Component {
           </div>
           <div className="row-span-1 ">
             <div className=" grid grid-cols-4 ">
-              <div className="col-span-2 flex flex-row">
-                <p className="text-1xl font-bold">Q:</p>
+              <div className="col-span-2 flex flex-row space-x-1">
+                <p className="text-1xl text-black font-bold">Q:</p>
                 <p>Who what which when where why where how?</p>
               </div>
-              <div className="col-span-2">Helpful? Yes(25) | Add Answer</div>
+              <div className="col-span-2 ">
+               <span className="text-xs font-thin space-x-1.5"> Helpful? Yes(25) | </span>
+                <span className="underline text-xs font-thin space-x-1.5">Add Answer</span>
+              </div>
             </div>
-            <div className=" grid grid-rows-2 ">
-              <div className="row-span-1 flex flex-row">
-                <p className="text-1xl font-bold">A:</p>
+            <div className=" grid grid-rows-2 space-x-1.5">
+              <div className="row-span-1 flex flex-row space-x-1.5">
+                <p className=" text-1xl text-black font-bold ">A:</p>
+                <span/>
                 Icing macaroon bear claw jelly beans chocolate cake.Cookie oat
                 cake chocolate halvah jelly cake cotton candy soufflé
                 topping.Jujubes topping cake gummies lemon drops.
               </div>
               <div className="row-span-1">
-                by User1337,May 1,2019 | Helpful? Yes (0) | Report
+               <span className="text-xs font-thin space-x-1.5">by User1337,May 1,2019</span>  
+               <span className="text-xs font-thin space-x-1.5">| Helpful?</span> 
+               <span className="underline text-xs font-thin space-x-1.5"> Yes </span>
+               <span className="no-underline text-xs font-thin space-x-1.5">(0)</span> <span className="underline text-xs font-thin space-x-1.5">| Report</span> 
               </div>
             </div>
           </div>
           <div className="row-span-1 ">
-            <div className=" grid grid-cols-4 ">
-              <div className="col-span-2 flex flex-row">
-                <p className="text-1xl font-bold">Q:</p>
+            <div className=" grid grid-cols-4 space-x-1">
+              <div className="col-span-2 flex flex-row space-x-1">
+                <p className="text-1xl font-bold ">Q:</p>
                 <p>whomst whatever whence wherefore?</p>
               </div>
-              <div className="col-span-2">Helpful? Yes(25) | Add Answer</div>
+              <div className="col-span-2">
+              <span className="text-xs font-thin space-x-1.5"> Helpful? Yes(25) | </span>
+                <span className="underline text-xs font-thin space-x-1.5">Add Answer</span>
+              </div>
             </div>
-            <div className=" grid grid-rows-2 ">
-              <div className="row-span-1 flex flex-row">
-                <p className="text-1xl font-bold">A:</p>
+            <div className=" grid grid-rows-2 space-x-1">
+              <div className="row-span-1 flex flex-row space-x-1">
+                <p className="text-1xl font-bold ">A:</p><span/>
                 Tootsie rool pudding jelly-o jujubes jelly-o wafer biscuit
                 danish ice cream.Cootton candy sweet roll candy.
               </div>
               <div className="row-span-1">
-                by User1337,May 1,2019 | Helpful? Yes (0) | Report
+              <span className="text-xs font-thin space-x-1.5">by User1337,May 1,2019</span>  
+               <span className="text-xs font-thin space-x-1.5">| Helpful?</span> 
+               <span className="underline text-xs font-thin space-x-1.5"> Yes </span>
+               <span className="no-underline text-xs font-thin space-x-1.5">(0)</span> <span className="underline text-xs font-thin space-x-1.5">| Report</span>
               </div>
             </div>
           </div>
           <div className="row-span-1 ">
-            <div className=" grid grid-cols-4 ">
-              <div className="col-span-2 flex flex-row">
+            <div className=" grid grid-cols-4 space-x-1">
+              <div className="col-span-2 flex flex-row space-x-1">
                 <p className="text-1xl font-bold">Q:</p>
                 <p>whomst whatever whence wherefore?</p>
               </div>
-              <div className="col-span-2">Helpful? Yes(25) | Add Answer</div>
+              <div className="col-span-2">
+              <span className="text-xs font-thin space-x-1.5"> Helpful? Yes(25) | </span>
+                <span className="underline text-xs font-thin space-x-1.5">Add Answer</span>
+              </div>
             </div>
-            <div className=" grid grid-rows-2 ">
-              <div className="row-span-1 flex flex-row">
-                <p className="text-1xl font-bold">A:</p>
+            <div className=" grid grid-rows-2 space-x-1 ">
+              <div className="row-span-1 flex flex-row space-x-1">
+                <p className="text-1xl font-bold space-x-1">A:</p>
+                <span/>
                 Tootsie rool pudding jelly-o jujubes jelly-o wafer biscuit
                 danish ice cream.Cootton candy sweet roll candy.
               </div>
               <div className="row-span-1">
-                by User1337,May 1,2019 | Helpful? Yes (0) | Report
+              <span className="text-xs font-thin space-x-1.5">by User1337,May 1,2019</span>  
+               <span className="text-xs font-thin space-x-1.5">| Helpful?</span> 
+               <span className="underline text-xs font-thin space-x-1.5"> Yes </span>
+               <span className="no-underline text-xs font-thin space-x-1.5">(0)</span> <span className="underline text-xs font-thin space-x-1.5">| Report</span>
               </div>
             </div>
           </div>
@@ -131,12 +151,18 @@ class QandA extends Component {
                   <img className="w-14 h-12 " />
                 </div>
                 <div className=" col-span-2">
-                  by User1337-Seller,May 1,2019 | Helpful? Yes (0) | Report
+                <span className="text-xs font-thin space-x-1.5">by User1337-<span className="text-xs font-bold space-x-1">Seller</span>,May 1,2019</span>  
+               <span className="text-xs font-thin space-x-1.5">| Helpful?</span> 
+               <span className="underline text-xs font-thin space-x-1.5"> Yes </span>
+               <span className="no-underline text-xs font-thin space-x-1.5">(0)</span> <span className="underline text-xs font-thin space-x-1.5">| Report</span>
+                  
                 </div>
               </div>
 
-              <div className=" text-bold">LOAD MORE ANSWERS</div>
+              
             </div>
+            <br/>
+            <div className="text-xs font-bold">LOAD MORE ANSWERS</div>
           </div>
           <div className="row-span-1 ">
             <div className="grid grid-cols-7">
